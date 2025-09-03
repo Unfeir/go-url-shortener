5908dc9ecf91237078f9c282469160cf783f0842
Unfeir <unfeir@gmail.com>
2025-09-03 21:06:40 +0300

    init commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"restAPI/internal/config"
+	"restAPI/internal/http-server/handlers/deleter"
+	"restAPI/internal/http-server/handlers/redirect"
+	"restAPI/internal/http-server/handlers/url/save"
+	mwLogger "restAPI/internal/http-server/middleware/logger"
+	"restAPI/internal/lib/logger/sl"
+	"restAPI/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// Підвантажуємо .env
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env file found")
+	}
+
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
+	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+	log.Info("Starting url-shotener", slog.String("env", cfg.Env))
+	log.Debug("debug msgs are enabled")
+
+	// TODO: init storage: sqllite
+
+	storage, err := sqlite.NewStorage(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	
+
+	// TODO: init router: chi, "chi render"
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("restAPI", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.New(log, storage))
+		r.Delete("/{alias}", deleter.New(log, storage))
+
+	})
+
+	router.Get("/{alias}", redirect.New(log, storage))
+
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	// TODO: run server:
+	srv := &http.Server{
+		Addr: cfg.Address,
+		Handler: router,
+		ReadTimeout: cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout: cfg.HTTPServer.IdleTimeout,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+	log.Error("Stop server")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+
---
+++ internal/http-server/handlers/deleter/delete.go
@@ -0,0 +1,56 @@
+package deleter
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"log/slog"
+
+	resp "restAPI/internal/lib/api/response"
+	"restAPI/internal/lib/logger/sl"
+	"restAPI/internal/storage"
+)
+
+// URLGetter is an interface for getting url by alias.
+//
+//go:generate go run github.com/vektra/mockery/v2@latest --name=URLGetter
+type URLDeleter interface {
+	DeleteURL(alias string) error
+}
+
+func New(log *slog.Logger, urlDeleter URLDeleter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.delete.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+			render.JSON(w, r, resp.Error("invalid request"))
+			return
+		}
+
+		err := urlDeleter.DeleteURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Error("url not found", "alias", alias)
+			render.JSON(w, r, resp.Error("not found"))
+			return
+		}
+
+		if err != nil {
+			log.Error("failed to get url", sl.Err(err))
+			render.JSON(w, r, resp.Error("internal error"))
+			return
+		}
+
+		log.Info("delete url for alias", slog.String("alias", alias))
+		render.NoContent(w,r)
+	}
+}
